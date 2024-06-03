263c58bc2f3cb4357a6d0618dbc85dcbc53259c9
Jim <jim@advancebass.com>
2024-06-03 12:02:47 -0700

    inspect process.env

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 
+console.log("process.env", process.env);
+
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to my Express App 🐱!!!</h1>`);
 });
